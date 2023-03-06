35cb813daca0de21001d912a7e7c761435cfc4af
Alvaro F <alvaro.fromero@gmail.com>
2023-03-06 07:00:32 +0100

    Remove non used variables

--- script.js
+++ script.js
@@ -407,17 +407,3 @@ btnSort.addEventListener("click", function (e) {
   sortState = !sortState;
   displayMovements(currentAccount, sortState);
 });
-
-// -----------------------------------------
-//                  MOVEMENTS
-// -----------------------------------------
-
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
